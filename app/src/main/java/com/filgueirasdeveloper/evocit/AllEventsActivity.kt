cf2490777fc4f5d6d53a64b140c09286b392e5da
João Filgueiras <joao.filgueiras>
2018-06-07 14:56:18 -0300

    #>Search Item

--- app/src/main/java/com/filgueirasdeveloper/evocit/AllEventsActivity.kt
+++ app/src/main/java/com/filgueirasdeveloper/evocit/AllEventsActivity.kt
@@ -2,6 +2,10 @@ package com.filgueirasdeveloper.evocit
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.view.MenuItemCompat
+import android.support.v7.widget.SearchView
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.ListView
 import com.filgueirasdeveloper.evocit.Adapter.Adapter
 import com.filgueirasdeveloper.evocit.DAO.DAOEvent
@@ -18,9 +22,52 @@ class AllEventsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_all_events)
-        listView = findViewById<ListView>(R.id.lista_eventos)
 
+        listView = findViewById<ListView>(R.id.lista_eventos)
+        setListView()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_principal, menu)
+        searchEvent(menu)
+        return true
+    }
+
+    fun searchEvent(menu: Menu?){
+        var searchItem : MenuItem? = menu?.findItem(R.id.action_search)
+        var searchView: SearchView = MenuItemCompat.getActionView(searchItem) as SearchView
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                setListView(query)
+                return false
+            }
+
+            override fun onQueryTextChange(query: String?): Boolean {
+                setListView(query)
+                return false
+            }
+
+        })
+    }
+
+    fun setListView(){
         var eventList: List<Event> = daoEvent.queryBuilder().orderBy("titulo", false).query()
+
+        val adapter = Adapter(this, eventList)
+        listView?.adapter = adapter
+    }
+
+    fun setListView(query: String?){
+        var eventList: List<Event> = daoEvent.queryBuilder()
+                                             .orderBy("titulo", false)
+                                             .where()
+                                             .like("titulo", "%" + query + "%")
+                                             .or()
+                                             .like("endereco", "%" + query + "%")
+                                             .or()
+                                             .like("observacao", "%" + query + "%")
+                                             .query()
 
         val adapter = Adapter(this, eventList)
         listView?.adapter = adapter
